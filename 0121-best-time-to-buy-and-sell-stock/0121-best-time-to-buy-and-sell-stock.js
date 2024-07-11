82ca40ef788a5ea6b91ef61910c2eacb011c37cd
Sam Bennett <SamBennettDev@gmail.com>
2024-07-11 14:16:14 -0600

    Time: 65 ms (90.83%), Space: 58.8 MB (64.36%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let max = 0;
+    let lowest = prices[0];
+
+    for (let i = 1; i < prices.length; i++) {
+        if (prices[i] < lowest) {
+            lowest = prices[i];
+        } else if (prices[i] - lowest > max) {
+            max = prices[i] - lowest;
+        }
+    }
+
+    return max;
+};
