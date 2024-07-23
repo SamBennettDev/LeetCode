c7ebe883b3be3c832226806cc687696610a00026
Sam Bennett <SamBennettDev@gmail.com>
2024-07-23 17:46:04 -0600

    Time: 66 ms (73.33%), Space: 50.8 MB (85.52%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function (s, t) {
+    if (s.length !== t.length) {
+        return false;
+    }
+
+    const count = {};
+
+    for (let i = 0; i < s.length; i++) {
+        count[s[i]] = (count[s[i]] || 0) + 1;
+    }
+
+    for (let i = 0; i < t.length; i++) {
+        if (!count[t[i]]) {
+            return false;
+        }
+        count[t[i]]--;
+    }
+
+    return true;
+}
